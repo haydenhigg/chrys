dde377cc79a86860dd67dc34ab9e9a7ee4433532
Hayden Higginbotham <hayden@higginbotham.io>
2025-08-16 13:46:23 -0400

    Put mean function in algo.go

--- algo/algo.go
+++ algo/algo.go
@@ -10,3 +10,13 @@ func Closes(candles []*candle.Candle) []float64 {
 
 	return closes
 }
+
+func mean(xs []float64) float64 {
+	sum := 0.
+
+	for _, x := range xs {
+		sum += x
+	}
+
+	return sum / float64(len(xs))
+}
