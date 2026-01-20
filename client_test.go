b266fd68e1771145d8f529c02428cc0b86cbb64b
Hayden Higginbotham <hayden@higginbotham.io>
2026-01-20 18:51:38 -0500

    Write test for TotalValue

--- client_test.go
+++ client_test.go
@@ -1,13 +1,59 @@
 package chrys
 
 import (
-	// "github.com/haydenhigg/chrys/driver"
+	"github.com/haydenhigg/chrys/frame"
 	"testing"
+	"time"
+	"math"
 )
 
 // mock
-type MockOrdererAPI struct {
+type MockAPI struct {
 	callback func()
+}
+
+func (api MockAPI) FetchBalances() (map[string]float64, error) {
+	return map[string]float64{
+		"USD": 133.7,
+		"BTC": 0.001337,
+		"ETH": 0.01337,
+	}, nil
+}
+
+func (api MockAPI) FetchFramesSince(
+	pair string,
+	interval time.Duration,
+	since time.Time,
+) ([]*frame.Frame, error) {
+	frames := []*frame.Frame{}
+	i := 0.
+
+	price := 0.
+	if pair == "BTC/USD" {
+		price = 88304.55
+	} else if pair == "ETH/USD" {
+		price = 2943.89
+	}
+
+	// all frames that start !Before(since) and end !After(now-interval)
+	now := time.Now().Add(-interval)
+	for t := since; !t.After(now); t = t.Add(interval) {
+		if t.Equal(t.Truncate(interval)) {
+			frames = append(frames, &frame.Frame{
+				Time: t,
+				Close: price + i,
+			})
+			i++
+		} else {
+			t = t.Truncate(interval)
+		}
+	}
+
+	return frames, nil
+}
+
+func (api MockAPI) MarketOrder(side, pair string, quantity float64) error {
+	return nil
 }
 
 // tests
@@ -40,4 +86,21 @@ func Test_SetIsLive(t *testing.T) {
 	if !client.IsLive {
 		t.Errorf("client.Fee != true: %v", client.IsLive)
 	}
+}
+
+func Test_TotalValue(t *testing.T) {
+	// create Client
+	client := NewClient(MockAPI{})
+
+	// TotalValue()
+	value, err := client.TotalValue("USD", time.Now())
+	if err != nil {
+		t.Errorf("err: %v", err)
+	}
+
+	// assert
+	if math.Abs(value - 291.12299265) > 10e-6 {
+		t.Errorf("value != 291.12299265: %f", value)
+	}
+
 }
