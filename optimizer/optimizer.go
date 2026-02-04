a4565d1415a709bc4c45aa7d592df1b811093e97
Hayden Higginbotham <hayden@higginbotham.io>
2026-02-04 18:29:03 -0500

    Tidy up Optimizer and move to own subpackage

---
+++ optimizer/optimizer.go
@@ -0,0 +1,138 @@
+package optimizer
+
+import (
+	"math"
+	// "fmt"
+	"github.com/haydenhigg/chrys/algo"
+	"maps"
+)
+
+type Input = map[string]float64
+type ObjectiveFunc = func(Input) float64
+
+type Optimizer struct {
+	F ObjectiveFunc
+	x Input
+}
+
+// initializer
+func New(objective ObjectiveFunc) *Optimizer {
+	return &Optimizer{
+		F: objective,
+		x: Input{},
+	}
+}
+
+// X setters
+func (opt *Optimizer) SetX(x Input) *Optimizer {
+	for k, v := range x {
+		opt.x[k] = v
+	}
+
+	return opt
+}
+
+// X getter
+func (opt *Optimizer) X() Input {
+	x := make(Input, len(opt.x))
+	maps.Copy(x, opt.x)
+
+	return x
+}
+
+// sensitivity analysis
+func (opt *Optimizer) xPlus(k string, h float64) Input {
+	x := opt.X()
+	x[k] += h
+
+	return x
+}
+
+// OAT partial derivatives using the finite difference method
+func (opt *Optimizer) Derivative(stepSize float64) Input {
+	h := max(1e-7, stepSize)
+
+	derivatives := make(Input, len(opt.x))
+
+	for k := range opt.x {
+		forward := opt.F(opt.xPlus(k, h))
+		backward := opt.F(opt.xPlus(k, -h))
+
+		// (f(x + h) - f(x - h)) / |2h|
+		derivatives[k] = (forward - backward) / math.Abs(2*h)
+	}
+
+	return derivatives
+}
+
+// OAT partial second derivatives using the finite difference method
+func (opt *Optimizer) SecondDerivative(stepSize float64) Input {
+	h := max(1e-4, stepSize)
+	baseline := opt.F(opt.X())
+
+	derivatives := make(Input, len(opt.x))
+
+	for k := range opt.x {
+		forward := opt.F(opt.xPlus(k, h))
+		backward := opt.F(opt.xPlus(k, -h))
+
+		// (f(x + h) - 2f(x) + f(x - h)) / h^2
+		derivatives[k] = (forward - 2*baseline + backward) / math.Pow(h, 2)
+	}
+
+	return derivatives
+}
+
+// OAT partial second derivatives using the finite difference method, averaged over multiple scales
+func (opt *Optimizer) SmoothedSecondDerivative(stepSize float64, n int) Input {
+	baseline := opt.F(opt.X())
+	derivatives := make(map[string][]float64, len(opt.x))
+
+	for k := range opt.x {
+		derivatives[k] = make([]float64, n)
+
+		h := max(1e-4, stepSize)
+
+
+		for i := range n {
+			forward := opt.F(opt.xPlus(k, h)) - baseline
+			backward := opt.F(opt.xPlus(k, -h)) - baseline
+
+			// (f(x + h) - 2f(x) + f(x - h)) / h^2
+			derivatives[k][i] = (forward + backward) / math.Pow(h, 2)
+
+			h *= 2
+		}
+	}
+
+	smoothedDerivatives := make(Input, len(opt.x))
+
+	for k, vs := range derivatives {
+		smoothedDerivatives[k] = algo.Mean(vs)
+	}
+
+	return smoothedDerivatives
+}
+
+// optimization
+func (opt *Optimizer) GradientDescent(learningRate float64, maxEpochs int) Input {
+	for _ = range maxEpochs {
+		shouldStop := true
+		for k, partialGradient := range opt.Derivative(0) {
+			if partialGradient == 0 {
+				continue
+			}
+
+			// descend down the gradient
+			opt.x[k] -= partialGradient * learningRate
+			shouldStop = false
+		}
+
+		// stop early if gradient == 0
+		if shouldStop {
+			break
+		}
+	}
+
+	return opt.X()
+}
---
+++ optimizer/optimizer_test.go
@@ -0,0 +1,137 @@
+package optimizer
+
+import (
+	"math"
+	"math/rand"
+	"testing"
+)
+
+// helpers
+func assertParametersEqual(a, b Input, t *testing.T) {
+	for k, va := range a {
+		if vb, ok := b[k]; !ok {
+			t.Errorf(`b["%s"] does not exist`, k)
+		} else if math.Abs(va-vb) > 1e-6 {
+			t.Errorf(`a["%s"] != b["%s"]: %v != %v`, k, k, va, vb)
+		}
+	}
+
+	for k := range b {
+		if _, ok := a[k]; !ok {
+			t.Errorf(`a["%s"] does not exist`, k)
+		}
+	}
+}
+
+// tests
+// tests -> X getter/setter
+func Test_SetX(t *testing.T) {
+	// create Optimizer
+	opt := New(func(x Input) float64 { return 0 })
+
+	// SetX()
+	x := Input{"abc": 13.37}
+	opt.SetX(x)
+
+	// assert
+	assertParametersEqual(opt.x, x, t)
+}
+
+func Test_X(t *testing.T) {
+	// create Optimizer
+	opt := New(func(x Input) float64 { return 0 })
+
+	opt.SetX(Input{"a": 4.1337})
+	opt.SetX(Input{"b": 3.37})
+
+	// X()
+	x := opt.X()
+
+	// assert
+	assertParametersEqual(x, Input{"a": 4.1337, "b": 3.37}, t)
+}
+
+// tests -> xPlus
+func Test_xPlus(t *testing.T) {
+	// create Optimizer
+	opt := New(func(x Input) float64 { return 0 })
+
+	opt.SetX(Input{"a": 4.1337, "b": 3.337})
+
+	// perturb()
+	x := opt.xPlus("b", 2e-3)
+
+	// assert
+	assertParametersEqual(x, Input{"a": 4.1337, "b": 3.339}, t)
+}
+
+// tests -> derivatives
+func Test_Derivative(t *testing.T) {
+	// create Optimizer
+	opt := New(func(x Input) float64 {
+		return math.Pow(x["a"], 2) + x["b"]
+	})
+
+	opt.SetX(Input{"a": 4, "b": 3})
+
+	// Derivative()
+	derivative := opt.Derivative(0)
+
+	// assert
+	// f(a, b) = a^2 + b
+	// f'(a) = 2a = 4, f'(b) = 1
+	assertParametersEqual(derivative, Input{"a": 8, "b": 1}, t)
+}
+
+func Test_SecondDerivative(t *testing.T) {
+	// create Optimizer
+	opt := New(func(x Input) float64 {
+		return math.Pow(x["a"], 2) - 3*x["a"] + math.Sin(x["b"])
+	})
+
+	opt.SetX(Input{"a": 4, "b": 3})
+
+	// SecondDerivative()
+	sensitivity := opt.SecondDerivative(0)
+
+	// assert
+	// f(a, b) = a^2 - 3a + sin(b)
+	// f'(a) = 2a - 3, f'(b) = cos(b)
+	// f''(a) = 2, f''(b) = -sin(b)
+	assertParametersEqual(sensitivity, Input{"a": 2, "b": -0.14112000806}, t)
+}
+
+func Test_SmoothedSecondDerivative(t *testing.T) {
+	// create Optimizer
+	opt := New(func(x Input) float64 {
+		noise := (rand.Float64() - 0.5)*1e-7
+		return math.Pow(x["a"], 3) - 3*x["a"] + math.Pow(x["b"], 2) + noise
+	})
+
+	opt.SetX(Input{"a": 4, "b": 3})
+
+	// SecondDerivative()
+	sensitivity := opt.SmoothedSecondDerivative(1, 20)
+
+	// assert
+	// f(a, b) = a^3 - 3a + x^2
+	// f'(a) = 3a^2 - 3, f'(b) = 2x
+	// f''(a) = 6a, f''(b) = 2
+	assertParametersEqual(sensitivity, Input{"a": 24, "b": 2}, t)
+}
+
+// tests -> GradientDescent
+func Test_GradientDescent(t *testing.T) {
+	// create Optimizer
+	opt := New(func(x Input) float64 {
+		return math.Pow(x["a"], 2) - 3*x["a"] + math.Sin(x["b"])
+	})
+
+	opt.SetX(Input{"a": 4, "b": 3})
+
+	// GradientDescent()
+	optimized := opt.GradientDescent(.1, 1000)
+
+	// assert
+	assertParametersEqual(optimized, Input{"a": 1.5, "b": 4.7123890}, t)
+}
