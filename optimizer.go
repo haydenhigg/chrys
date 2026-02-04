3b7d8622c085e7b83a4dddbed1e5f90a1869c0cf
Hayden Higginbotham <hayden@higginbotham.io>
2026-02-03 19:10:55 -0500

    Make new parameter model

--- optimizer.go
+++ optimizer.go
@@ -1,74 +1,135 @@
 package chrys
 
-import "math"
+import (
+	"math"
+	// "math/rand"
+	"maps"
+)
 
-// a single-objective optimizing object
+type Parameters = map[string]float64
+type ObjectiveFunc = func(Parameters) float64
+
+type Domain struct {
+	Lower,
+	Upper,
+	Resolution float64
+}
+
 type Optimizer struct {
-	inputs []float64
+	F ObjectiveFunc
+
+	x map[string]float64
+	xDomain map[string]*Domain
 }
 
-func NewOptimizer(inputs []float64) *Optimizer {
-	return &Optimizer{inputs: inputs}
+// initializer
+func NewOptimizer(objective ObjectiveFunc) *Optimizer {
+	return &Optimizer{
+		F: objective,
+		x: map[string]float64{},
+		xDomain: map[string]*Domain{},
+	}
 }
 
-// Copy and Perturb
-func (opt *Optimizer) X(deltas ...float64) []float64 {
-	x := make([]float64, len(opt.inputs))
-	copy(x, opt.inputs)
+// X setters
+func (opt *Optimizer) CreateX(
+	k string, v float64,
+	lowerBound float64,
+	upperBound float64,
+	resolution float64,
+) *Optimizer {
+	opt.SetX(k, v)
+	opt.xDomain[k] = &Domain{lowerBound, upperBound, math.Abs(resolution)}
 
-	for i := range min(len(x), len(deltas)) {
-		x[i] += deltas[i]
+	return opt
+}
+
+func (opt *Optimizer) SetX(k string, v float64) *Optimizer {
+	opt.x[k] = v
+	return opt
+}
+
+// X getter
+func (opt *Optimizer) X() Parameters {
+	x := make(Parameters, len(opt.x))
+	maps.Copy(x, opt.x)
+
+	return x
+}
+
+func (opt *Optimizer) Domain(k string) *Domain {
+	if domain, ok := opt.xDomain[k]; ok {
+		return domain
+	} else {
+		return &Domain{math.Inf(-1), math.Inf(1), 1e-8}
+	}
+}
+
+func (opt *Optimizer) XPerturb(k string, h float64) Parameters {
+	domain := opt.Domain(k)
+
+	x := opt.X()
+	x[k] += max(h, domain.Resolution)
+
+	// clamp to bounds
+	if x[k] > domain.Upper {
+		x[k] = domain.Upper
+	} else if x[k] < domain.Lower {
+		x[k] = domain.Lower
 	}
 
 	return x
 }
 
 // OAT partial derivatives using the finite difference method
-func (opt *Optimizer) Derivative(f func([]float64) float64) []float64 {
-	h := 1e-5
-
-	derivatives := make([]float64, len(opt.inputs))
-	for i := range opt.inputs {
-		x := opt.X()
-		dx := 2 * h
-
-		x[i] += h
-		forward := f(x)
-
-		x[i] -= dx
-		backward := f(x)
+func (opt *Optimizer) Derivative() Parameters {
+	derivatives := make(Parameters, len(opt.x))
+	for k := range opt.x {
+		h := opt.Domain(k).Resolution
+		forward := opt.F(opt.XPerturb(k, h))
+		backward := opt.F(opt.XPerturb(k, -h))
 
 		// (f(x + h) - f(x - h)) / 2h
-		derivatives[i] = (forward - backward) / math.Abs(dx)
+		derivatives[k] = (forward - backward) / math.Abs(2 * h)
 	}
 
 	return derivatives
 }
 
-// OAT local perturbations, very much like a second derivative but not quite
-func (opt *Optimizer) LocalSensitivity(f func([]float64) float64) []float64 {
-	// epsilons := make([]float64, len(opt.inputs))
-	// for i, x := range opt.inputs {
-	// 	epsilons[i] = x * 1e-3
+// OAT local perturbations, very much like a derivative but not quite
+func (opt *Optimizer) LocalSensitivity() Parameters {
+	// isLowerBoundInf := math.IsInf(lowerBound, 0)
+	// isUpperBoundInf := math.IsInf(upperBound, 0)
+
+	// h := resolution
+	// if isLowerBoundInf && isUpperBoundInf {
+
+	// } else if isLowerBoundInf {
+	// } else if isUpperBoundInf {
+	// } else {
 	// }
 
-	h := 1e-3
+	// math.IsInf(lowerBound, -1)
+	// math.IsInf(domain[0], 1)
+	// h := rand.Float64()
 
-	baseline := f(opt.X())
-	sensitivities := make([]float64, len(opt.inputs))
-	for i := range opt.inputs {
-		x := opt.X()
-		dx := 2 * h
+	// // // //
 
-		x[i] += h
-		plus := f(x) - baseline
+	// baseline := f(opt.X())
+	sensitivities := make(Parameters, len(opt.x))
+	// for i := range opt.inputs {
+	// 	x := opt.X()
+	// 	dx := 2 * h
 
-		x[i] -= dx
-		minus := f(x) - baseline
+	// 	x[i] += h
+	// 	plus := f(x) - baseline
 
-		// (|f(x + h) - f(x)| + |f(x - h) - f(x)|) / 2h
-		sensitivities[i] = (math.Abs(plus) + math.Abs(minus)) / math.Abs(dx)
-	}
+	// 	x[i] -= dx
+	// 	minus := f(x) - baseline
+
+	// 	// (|f(x + h) - f(x)| + |f(x - h) - f(x)|) / 2h
+	// 	sensitivities[i] = (math.Abs(plus) + math.Abs(minus)) / math.Abs(dx)
+	// }
 
 	return sensitivities
 }
--- optimizer_test.go
+++ optimizer_test.go
@@ -3,85 +3,194 @@ package chrys
 import (
 	"math"
 	"testing"
+	"fmt"
 )
+
+// helpers
+func assertParametersEqual(a, b Parameters, t *testing.T) {
+	for k, va := range a {
+		if vb, ok := b[k]; !ok {
+			t.Errorf(`b["%s"] does not exist`, k)
+		} else if math.Abs(va-vb) > 1e-6 {
+			t.Errorf(`a["%s"] != b["%s"]: %v != %v`, k, k, va, vb)
+		}
+	}
+
+	for k := range b {
+		if _, ok := a[k]; !ok {
+			t.Errorf(`a["%s"] does not exist`, k)
+		}
+	}
+}
+
+func assertDomainsEqual(a, b *Domain, t *testing.T) {
+	if math.Abs(a.Lower - b.Lower) > 1e-6 {
+		t.Errorf(`a.Lower != b.Lower: %f != %f`, a.Lower, b.Lower)
+	}
+	if math.Abs(a.Upper - b.Upper) > 1e-6 {
+		t.Errorf(`a.Upper != b.Upper: %f != %f`, a.Upper, b.Upper)
+	}
+	if math.Abs(a.Resolution - b.Resolution) > 1e-6 {
+		t.Errorf(
+			`a.Resolution != b.Resolution: %f != %f`,
+			a.Resolution, b.Resolution,
+		)
+	}
+}
+
+func assertDomainMapsEqual(a, b map[string]*Domain, t *testing.T) {
+	for k, va := range a {
+		if vb, ok := b[k]; !ok {
+			t.Errorf(`b["%s"] does not exist`, k)
+		} else {
+			assertDomainsEqual(va, vb, t)
+		}
+	}
+
+	for k := range b {
+		if _, ok := a[k]; !ok {
+			t.Errorf(`a["%s"] does not exist`, k)
+		}
+	}
+}
+
+// tests
+// tests -> CreateX
+func Test_CreateX(t *testing.T) {
+	// create Optimizer
+	opt := NewOptimizer(func(x Parameters) float64 { return 0 })
+
+	// CreateX()
+	opt.CreateX("abc", -13.37, math.Inf(-1), 0, 1e-2)
+
+	// assert
+	assertParametersEqual(opt.x, Parameters{"abc": -13.37}, t)
+	assertDomainMapsEqual(opt.xDomain, map[string]*Domain{
+		"abc": {math.Inf(-1), 0, 1e-2},
+	}, t)
+}
+
+// tests -> SetX
+func Test_SetX(t *testing.T) {
+	// create Optimizer
+	opt := NewOptimizer(func(x Parameters) float64 { return 0 })
+
+	// SetX()
+	opt.SetX("abc", -13.37)
+
+	// assert
+	assertParametersEqual(opt.x, Parameters{"abc": -13.37}, t)
+	assertDomainMapsEqual(opt.xDomain, map[string]*Domain{}, t)
+}
 
 // tests -> X
 func Test_X(t *testing.T) {
 	// create Optimizer
-	opt := NewOptimizer([]float64{4.1337, 3.37})
+	opt := NewOptimizer(func(x Parameters) float64 { return 0 })
+
+	opt.SetX("a", 4.1337)
+	opt.SetX("b", 3.37)
 
 	// X()
 	x := opt.X()
 
 	// assert
-	assertSlicesEqual(x, []float64{4.1337, 3.37}, t)
+	assertParametersEqual(x, Parameters{"a": 4.1337, "b": 3.37}, t)
 }
 
-func Test_XPerturbed(t *testing.T) {
+// tests -> Domain
+func Test_Domain(t *testing.T) {
 	// create Optimizer
-	opt := NewOptimizer([]float64{4.1337, 3.37})
+	opt := NewOptimizer(func(x Parameters) float64 { return 0 })
 
-	// X()
-	x := opt.X(-.1337, .63)
+	opt.CreateX("abc", -13.37, math.Inf(-1), 0, 1e-2)
+
+	// Domain()
+	domain := opt.Domain("abc")
 
 	// assert
-	assertSlicesEqual(x, []float64{4, 4}, t)
+	assertDomainsEqual(domain, &Domain{math.Inf(-1), 0, 1e-2}, t)
 }
 
-func Test_XPerturbedTooFew(t *testing.T) {
+func Test_DomainDefault(t *testing.T) {
 	// create Optimizer
-	opt := NewOptimizer([]float64{4.1337, 3.37})
+	opt := NewOptimizer(func(x Parameters) float64 { return 0 })
+
+	opt.SetX("abc", -13.37)
 
 	// X()
-	x := opt.X(-.1337)
+	domain := opt.Domain("abc")
 
 	// assert
-	assertSlicesEqual(x, []float64{4, 3.37}, t)
+	assertDomainsEqual(domain, &Domain{math.Inf(-1), math.Inf(1), 1e-8}, t)
 }
 
-func Test_XPerturbedTooMany(t *testing.T) {
+// tests -> XPerturb
+func Test_XPerturb(t *testing.T) {
 	// create Optimizer
-	opt := NewOptimizer([]float64{4.1337, 3.37})
+	opt := NewOptimizer(func(x Parameters) float64 { return 0 })
 
-	// X()
-	x := opt.X(-.1337, .63, 2.5)
+	opt.SetX("a", 4.1337)
+	opt.CreateX("b", 3.337, 0, math.Inf(1), 1e-3)
+
+	// XPerturb()
+	x := opt.XPerturb("b", 2e-3)
 
 	// assert
-	assertSlicesEqual(x, []float64{4, 4}, t)
+	assertParametersEqual(x, Parameters{"a": 4.1337, "b": 3.339}, t)
+}
+
+func Test_XPerturbOutsideBounds(t *testing.T) {
+	// create Optimizer
+	opt := NewOptimizer(func(x Parameters) float64 { return 0 })
+
+	opt.SetX("a", 4.1337)
+	opt.CreateX("b", 3.337, 0, 3.338, 1e-3)
+
+	// XPerturb()
+	x := opt.XPerturb("b", 2e-3)
+
+	// assert
+	assertParametersEqual(x, Parameters{"a": 4.1337, "b": 3.338}, t)
 }
 
 // tests -> Derivative
 func Test_Derivative(t *testing.T) {
 	// mock objective
-	f := func(x []float64) float64 {
-		return math.Pow(x[0], 2) + x[1]
+	f := func(x Parameters) float64 {
+		return math.Pow(x["a"], 2) + x["b"]
 	}
 
 	// create Optimizer
-	opt := NewOptimizer([]float64{4, 3})
+	opt := NewOptimizer(f)
+
+	opt.SetX("a", 4)
+	opt.SetX("b", 3)
 
 	// Derivative()
-	fPrime := opt.Derivative(f)
+	derivative := opt.Derivative()
+
+	fmt.Println(derivative)
 
 	// assert
 	// f(a, b) = a^2 + b
 	// f'(a) = 2a = 4, f'(b) = 1
-	assertSlicesEqual(fPrime, []float64{8, 1}, t)
+	assertParametersEqual(derivative, Parameters{"a": 8, "b": 1}, t)
 }
 
-// tests ->
-func Test_LocalSensitivity(t *testing.T) {
-	// mock objective
-	f := func(x []float64) float64 {
-		return math.Pow(x[0], 2) + x[1]
-	}
+// tests -> LocalSensitivity
+// func Test_LocalSensitivity(t *testing.T) {
+// 	// mock objective
+// 	f := func(x []float64) float64 {
+// 		return math.Pow(x[0], 2) + x[1]
+// 	}
 
-	// create Optimizer
-	opt := NewOptimizer([]float64{4, 3})
+// 	// create Optimizer
+// 	opt := NewOptimizer([]float64{4, 3})
 
-	// Derivative()
-	sens := opt.LocalSensitivity(f)
+// 	// Derivative()
+// 	sens := opt.LocalSensitivity(f)
 
-	// assert
-	assertSlicesEqual(sens, []float64{.32, .03}, t)
-}
+// 	// assert
+// 	assertSlicesEqual(sens, []float64{.32, .03}, t)
+// }
